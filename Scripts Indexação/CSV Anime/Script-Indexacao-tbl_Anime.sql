1775f7e7a80979322a8e19258824a234a3bc6e71
Obunt <silva_daniel28@yahoo.com>
2025-12-08 11:34:48 -0300

    feat: Criação do índice na table score. Criação de um índice para a chave FK na table score.

--- Scripts Indexação/CSV Anime/Script-Indexacao-tbl_Anime.sql
+++ Scripts Indexação/CSV Anime/Script-Indexacao-tbl_Anime.sql
@@ -1,9 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 
+--Índice do Nome
 create index idx_anime_name on "Projeto-Anime".anime using gin(name gin_trgm_ops);
 
+--Índice do Nome em Inglês
 create index idx_anime_english_name on "Projeto-Anime".anime using gin(english_name gin_trgm_ops);
 
+--Índice do nome em Japonês
 create index idx_anime_japanese_name on "Projeto-Anime".anime using gin(japanese_name gin_trgm_ops);
 
 SELECT * FROM anime WHERE name ILIKE '%Boku%';
---
+++ Scripts Indexação/CSV Anime/Script-Indexacao-tbl_Score.sql
@@ -0,0 +1,3 @@
+--Índice da FK
+CREATE INDEX idx_score_anime_id
+ON "Projeto-Anime".score(anime_id);
