8a8c650acdf83c3da03f87032e572b242c681518
Pedro aleatório <pedrohenriquecosta2612@gmail.com>
2025-12-11 19:05:29 -0300

    nomenclatura de pasta

---
+++ Scripts Normalização/CSV anime_with_synopsis/Script-Insercao-Dados.sql
@@ -0,0 +1,7 @@
+--inserção de dados
+insert into "Projeto-Anime".anime_with_synopsis 
+	(MAL_ID, synopsis)
+select mal_id, sypnopsis 
+	from "Projeto-Anime".csv_a_w_s csv_aws
+
+	ON CONFLICT DO NOTHING;
--- Scripts Normalização/CSV anime_with_sypnopsis/Script-Insercao-Dados.sql
+++
@@ -1,7 +0,0 @@
---inserção de dados
-insert into "Projeto-Anime".anime_with_synopsis 
-	(MAL_ID, synopsis)
-select mal_id, sypnopsis 
-	from "Projeto-Anime".csv_a_w_s csv_aws
-
-	ON CONFLICT DO NOTHING;
