f673aa5ddbaff902d4d8e5cc7b749e3a56c2883a
Matheus Luiz Rocha <matheuslr2@gmail.com>
2025-12-11 14:19:22 -0300

    "Adicionando trigger 'trg_atualizar_status' na pasta 'OBJETOS'"

---
+++ Objetos/Trigger/trg_atualizar_status.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION fn_atualizar_status()
+RETURNS TRIGGER AS $$
+DECLARE
+	total_eps INT;
+BEGIN
+    -- A trigger não vai sobrescrever a opção 3 = "ON-HOLD"
+	IF OLD.status IN (3) THEN 
+		RETURN NEW;
+	END IF;
+
+	
+	-- Puxando todos os episódios dos animes
+	SELECT
+		a.episodes
+		INTO total_eps
+	FROM anime a
+	WHERE mal_id = NEW.anime_id;
+
+
+	-- Atualizando status para completo (COMPLETED)
+	IF NEW.watched_episodes = total_eps THEN
+		NEW.status := 2;
+
+	-- Atualizando status para assistindo (CURRENTLY WATCHING)
+	ELSIF NEW.watched_episodes > 0 AND NEW.watched_episodes < total_eps THEN
+		NEW.status := 1;
+
+	-- Atualizando status para assistir futuramente (PLAN TO WATCH)
+	ELSIF NEW.watched_episodes = 0 THEN
+		NEW.status := 6;
+	END IF;
+	
+	RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER trg_atualizar_status
+BEFORE UPDATE OF watched_episodes 
+ON animelist_normalizada
+FOR EACH ROW 
+EXECUTE FUNCTION fn_atualizar_status();
